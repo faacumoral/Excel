041728836ff94d6fa7c79d54eb54607ce2d13991
Facundo Moral <facundo.moral@snoopconsulting.com>
2018-07-31 16:34:59 -0300

    agrego helpers

---
+++ ExcelCreator/ExcelHelpers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExcelCreator
+{
+    public class ExcelHelpers
+    {
+        public static List<ExcelCelda> GetCeldas(params object[] valores)
+        {
+            return valores.Select(v => new ExcelCelda { Valor = v.ToString() }).ToList();
+        }
+    }
+}
